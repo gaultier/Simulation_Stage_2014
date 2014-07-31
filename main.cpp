ba4314f0de3f351901fe35f033f5dfb44cd91ec9
Philippe <philigaultier@gmail.com>
2014-07-31 12:20:00 +0200

    Add help message

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char** argv)
 {
     bool fullscreen = false;
     bool oculusRender = false;
+    bool help = false;
 
     logger->info(logger->get() << "Command line arguments: " << argc-1);
 
@@ -12,8 +13,18 @@ int main(int argc, char** argv)
     {
         std::string arg(argv[i]);
 
-        fullscreen = (arg == "-f");
-        oculusRender = (arg == "-o");
+        fullscreen = (arg == "-f" || arg == "--fullscreen");
+        oculusRender = (arg == "-o" || arg == "--oculus");
+        help = (arg == "-h" || arg == "--help");
+    }
+
+    if(help)
+    {
+        cout << "Usage: " << endl;
+        cout << "\t-o, --oculus: Oculus mode" << endl;
+        cout << "\t-f, --fullscreen: Full screen mode" << endl;
+        cout << "\t-h, --help: This help message" << endl;
+        return 0;
     }
 
     Scene scene("Simulation", WINDOW_WIDTH, WINDOW_HEIGHT, oculusRender, fullscreen);
